f2af510b306f86f85681bfa7cbcba431829f4f60
Traci <traci.face@gmail.com>
2021-07-11 22:13:13 -0400

    workout schema added

---
+++ models/workout.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+	day: {
+		type: Date,
+		default: () => new Date()
+	},
+	exercises: [
+		{
+			name: {
+				type: String,
+				trim: true,
+				required: "What is the name of your exercise"
+			},
+			type: {
+				type: String,
+				trim: true,
+				required: "What type of exercise is this?"
+			},
+			weight: {
+				type: Number
+			},
+			sets: {
+				type: Number
+			},
+			reps: {
+				type: Number
+			},
+			duration: {
+				type: Number,
+				required: "Enter an exercise duration in minutes"
+			},
+			distance: {
+				type: Number
+			}
+        }
+      ]
+},
+ {toJSON: {
+		virtuals: true
+	}
+});
+
+
+workoutSchema.virtual("totalDuration")
+	.get(function () {
+		return this.exercises.reduce((total, exercise) => {
+			return total + exercise.duration;
+		}, 0);
+	});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
